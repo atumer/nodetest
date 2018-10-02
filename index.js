e8ea4951b354485bf4c9c29c15bdea1bd4c99597
Arif TÜMER <atumer@innova.com.tr>
2018-10-02 17:17:27 +0300

    Updates

--- index.js
+++
@@ -1,9 +0,0 @@
-const http = require('http');
-
-const server = http.createServer((req, res) => {
-  res.end('Hello World He He\n');
-});
-
-server.listen(8080, () => {
-  console.log('Server is running...');
-});
